80d2dcba47b3bc15950e08715bb80d79f7843ead
tehhowch <tehhowchmh@gmail.com>
2017-07-28 14:07:58 -0500

    Working draft for AntiMissile

--- src/AntiMissile.cpp
+++ src/AntiMissile.cpp
@@ -1,20 +1,75 @@
-/*
-Class describing an anti-missile.
- */
+/* AntiMissile.cpp
+Copyright (c) 2017 by tehhowch
+
+AntiMissileComparator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+AntiMissileComparator is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+*/
 
 #include "AntiMissile.h"
 
+#include "DataNode.h"
 #include "Missile.h"
 #include "Random.h"
 
 #include <cmath>
-#include <string>
 
 using namespace std;
 
-void AntiMissile::Initialize()
+void AntiMissile::Load(const DataNode &node)
 {
+	if(node.Size() > 1)
+		name = node.Token(1);
+	
+	for(const DataNode &child : node)
+	{
+		if((child.Token(0) == "strength" || child.Token(0) == "anti-missile strength") && child.Size() > 1)
+			strength = child.Value(1);
+		else if(child.Token(0) == "range" && child.Size() > 1)
+			range = child.Value(1);
+		else if(child.Token(0) == "reload" && child.Size() > 1)
+			reload = child.Value(1);
+		else
+			child.PrintTrace("Skipping unrecognized attribute:");
+	}
+	
+	Initialize();
+}
+
+
+
+void AntiMissile::Initialize(const Point position)
+{
+	this->position = position;
 	cooldown = 0;
+}
+
+
+
+void AntiMissile::SetPosition(const Point position)
+{
+	this->position = position;
+}
+
+
+
+void AntiMissile::SetVelocity(const Point velocity)
+{
+	this->velocity = velocity;
+}
+
+
+
+void AntiMissile::Step()
+{
+	position += velocity;
+	cooldown = max(0, cooldown - 1);
 }
 
 
@@ -23,6 +78,7 @@ bool AntiMissile::CanFire(const Missile &missile) const
 {
 	return !cooldown && range >= (missile.Position() - position).Length();
 }
+
 
 
 // Reset the AM's cooldown and return the strength check used to see if
@@ -35,7 +91,7 @@ int AntiMissile::Fire()
 
 
 
-const std::string &AntiMissile::Name() const
+const string &AntiMissile::Name() const
 {
 	return name;
 }
@@ -59,13 +115,6 @@ const double AntiMissile::Range() const
 const double AntiMissile::ShotsPerSecond() const
 {
 	return 60. / max(reload, 1);
-}
-
-
-
-void AntiMissile::Step()
-{
-	cooldown = std::max(0, cooldown - 1);
 }
 
 
--- src/AntiMissile.h
+++ src/AntiMissile.h
@@ -1,6 +1,16 @@
-/*
-Class describing an anti-missile.
- */
+/* AntiMissile.h
+Copyright (c) 2017 by tehhowch
+
+AntiMissileComparator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+AntiMissileComparator is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+*/
 
 #ifndef ANTI_MISSILE_H_
 #define ANTI_MISSILE_H_
@@ -9,15 +19,23 @@ Class describing an anti-missile.
 
 #include <string>
 
+class DataNode;
 class Missile;
 class Point;
 
 class AntiMissile {
 public:
-	void Initialize();
-	bool CanFire(const Missile &missile) const;
-	int Fire();
+	void Load(const DataNode &node);
+	void Initialize(const Point position = Point());
+	void SetPosition(const Point position);
+	void SetVelocity(const Point velocity = Point());
+	
+	// Decrements cooldown and move the system by its current velocity.
 	void Step();
+	// Return true if the system is ready to fire and the missile is in range.
+	bool CanFire(const Missile &missile) const;
+	// Reset the cooldown and return the strength of the attack: [0, strength).
+	int Fire();
 	
 	// Get properties of this anti-missile outfit.
 	const std::string &Name() const;
@@ -29,11 +47,12 @@ public:
 private:
 	std::string name;
 	Point position;
+	Point velocity;
 
-	int strength = 0;
+	int cooldown = 5000;
 	double range = 0.;
 	int reload = 1;
-	int cooldown = 5000;
+	int strength = 0;
 };
 
 
